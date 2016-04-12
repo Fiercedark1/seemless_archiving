5c89c803b89ee1711037bd8f6d4804ac3cce03d8
Cloud User <centos@ip-172-31-14-25.us-west-2.compute.internal>
2016-04-12 06:18:25 +0000

    complete software for archiving and unarchiving files

---
+++ python_scripts/time_limit.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os
+import time
+import subprocess
+from datetime import datetime
+
+f  = os.listdir("/proj/3hr_limit/")
+p = "/proj/3hr_limit/"
+g = "/proj/tmp_arch/"
+now = time.time()
+three_hours = now - 3 * 3600
+
+print "Searching for files 3 hours or older........"
+for old_file in f:
+    file_age = os.stat("/proj/3hr_limit/"+old_file).st_mtime
+    if file_age < three_hours:
+        subprocess.call(["tar", "-cvf", g+old_file+".tar", p+old_file])
+        print old_file+" is 3 hours or older"
+        subprocess.call(["rm", p+old_file])
+        print old_file+" has been removed from " + p + "and moved to "+ g
+    else:
+        print old_file+" isn't 3 hours or older"
+print "Search complete... No files meet the criteria"        
